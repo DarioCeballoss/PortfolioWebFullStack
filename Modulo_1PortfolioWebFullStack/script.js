2de86af6e70214c44c2049f4d5ec5d50418dc1c3
Dario Ceballos <darioceballos75@gmail.com>
2022-05-17 16:34:30 -0300

    Reemplazo de los porcentajes por una imagen creando un elemento img desde JS

--- Modulo_1PortfolioWebFullStack/script.js
+++ Modulo_1PortfolioWebFullStack/script.js
@@ -1,14 +1,15 @@
 window.onload = function(){
-    progresCircule("circJS" , "porcJS" , 60 );
-    progresCircule("circHTML" , "porcHTML" , 30 );
-    progresCircule("circBootstrap" , "porcBootstrap" , 50 );
-    progresCircule("circAngular" , "porcAngular" , 60 );
-    progresCircule("circSQL" , "porcSQL" , 30 );
-    //progresCircule("circBootstrap" , "porcBootstrap" , 50 );
+    progresCircule("circCSS" , "porcCSS" , 30 , "img/LogoCSS.png" );
+    progresCircule("circHTML" , "porcHTML" , 30 , "img/LogoHTML.png" );
+    progresCircule("circJS" , "porcJS" , 60 , "img/LogoJS.png");
+    progresCircule("circBootstrap" , "porcBootstrap" , 50 , "img/LogoBootstrap.png" );
+    progresCircule("circAngular" , "porcAngular" , 60 , "img/LogoAngular.png" );
+    progresCircule("circSQL" , "porcSQL" , 30 , "img/LogoSQL.png" );
+  //progresCircule("circBootstrap" , "porcBootstrap" , 50 );
   }
   
   
-    function progresCircule(idCirculo , idPorcentaje , valPorcentaje){
+    function progresCircule(idCirculo , idPorcentaje , valPorcentaje, imgSkil){
       const can = document.getElementById(idCirculo),
           nProcent = document.getElementById(idPorcentaje),
           c = can.getContext('2d');
@@ -20,15 +21,20 @@ window.onload = function(){
         procent = valPorcentaje,     // valor del porcentaje en el medio TEXTO
         oneProcent = 360 / 100, //transforma el perimetro en porcentaje
         result = oneProcent * procent;// porcentaje de progreso 
+        
+        //IMAGEN EN EL CIRCULO
+        var x = document.createElement("IMG");
+        x.setAttribute("src", imgSkil);
+        x.setAttribute("width", "70")
   
-  
-   nProcent.innerHTML = procent + "%";
+        nProcent.appendChild(x);
+   //nProcent.innerHTML = procent + "%";
         
         c.clearRect( 0, 0, can.width, can.height );
   
         c.beginPath();
         c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + 360) );  // Circulo de abajo
-        c.strokeStyle = 'lightslategrey'; // color del anterior
+        c.strokeStyle = '#b1b1b3'; // color del anterior
         c.lineWidth = '15';// espesor del anterior
         c.stroke();
   
